a82f748819f5ad32bc4751419cb5049db4f67725
Angelo Filho <angelo.filho06@hotmail.com>
2023-01-18 18:39:28 -0300

    feat: add summary table component

---
+++ src/components/HabitDay.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from "react";
+
+// interface HabitDayProps {
+//   children: ReactNode;
+// }
+
+export function HabitDay() {
+  return (
+    <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
+  );
+}
